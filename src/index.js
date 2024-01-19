29da0046cd3e3207398bb6227995b6b98a9cf1d8
hrjha97 <jha.harsh837@gmail.com>
2024-01-20 01:33:10 +0530

    connected to mongoatlas

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path : './env'
+})
+connectDB();
+ 
+
+// using iffi function
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}
+//         /${DB_NAME}`);
+//         app.on("error",(error)=>{
+//             console.log("Error",error);
+           
+            
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on ${process.env.PORT}`)
+//         })
+        
+//     } catch (error) {
+//         console.log("Error : ",error);
+        
+        
+        
+//     }
+
+
+// })()
